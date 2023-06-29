82c98025741bd528ae02a2583223f33b016ea175
varshith varma datla <119720179+7337232446@users.noreply.github.com>
2023-06-29 16:44:16 +0530

    #include<stdio.h> int prime(int n) {     int c=0;     for(int i=1;i<=n;i++)     {         if(n%i==0)         {             c++;         }     }     if(c==2)     {         return 1;     }     else     {         return 0;     } } int main() {     int r;     scanf("%d",&r);     for(int i=1;i<=r;i++)     {         int n,f,b;         scanf("%d",&n);         for(int i=n;;i++)     {         if(prime(i))         {             f=i;             break;         }     }     for(int i=n;;i--)     {         if(prime(i))         {             b=i;             break;         }     }     int d1=f-n;     int d2=n-b;     if(d2<=d1)     {         printf("%d ",b);     }     else if(d1<=d2)     {         printf("%d ",f);     }     } }

--- Nearest_Prime.c
+++ Nearest_Prime.c
@@ -1,59 +1,58 @@
 #include<stdio.h>
-int is_prime(int);
-int pre_prime(int);
-int nex_prime(int);
+int prime(int n)
+{
+    int c=0;
+    for(int i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            c++;
+        }
+    }
+    if(c==2)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
 int main()
 {
-	int k,n,a,b,i;
-	scanf("%d",&k);
-	for (i=1;i<=k;i++)
-	{
-	scanf("%d",&n);
-	a= pre_prime(n);
-	b= nex_prime(n);
-	if ((n-a)<=(b-n))
-	{
-		printf("%d
-",a);
-		
-	}
-	else
-	{
-		printf("%d
+    int r;
+    scanf("%d",&r);
+    for(int i=1;i<=r;i++)
+    {
+        int n,f,b;
+        scanf("%d",&n);
+        for(int i=n;;i++)
+    {
+        if(prime(i))
+        {
+            f=i;
+            break;
+        }
+    }
+    for(int i=n;;i--)
+    {
+        if(prime(i))
+        {
+            b=i;
+            break;
+        }
+    }
+    int d1=f-n;
+    int d2=n-b;
+    if(d2<=d1)
+    {
+        printf("%d
 ",b);
-	}
-	}
-	
+    }
+    else if(d1<=d2)
+    {
+        printf("%d
+",f);
+    }
+    }  
 }
-int is_prime(int m)
-{
-		int	fc=0,i;
-	for(i=1;i<=m;i++)
-	{
-
-		if(m%i==0)
-		{
-			fc=fc+1;			
-		}
-	}
-	return fc==2;
-}
-
-
-int pre_prime(int n)
-{
-	while (is_prime(n) == 0)
-	{
-		n--;
-	}
-	return n;
-}
-int nex_prime(int n)
-{
-	while(is_prime(n)==0)
-	{
-		n++;
-	}
-	return n;
-}
-
