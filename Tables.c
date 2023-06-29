772c7e5470c6754c58825b9d7dcbbfd131b5d0c2
varshith varma datla <119720179+7337232446@users.noreply.github.com>
2023-06-29 19:36:43 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d%d",&j,&n);     for(i=1;i<=n;i++)     {         if(i%2!=0)         {             printf("%d x %d = %d ",j,i,j*i);         }     } }

--- Tables.c
+++ Tables.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,p,r;
-    scanf("%d%d",&n,&r);
-    for(i=1;i<=r;i++)
-{
-    if(i%2!=0)
+    int n,i,j;
+    scanf("%d%d",&j,&n);
+    for(i=1;i<=n;i++)
     {
-        p=n*i;
-        printf("%d x %d = %d
-",n,i,p);
+        if(i%2!=0)
+        {
+            printf("%d x %d = %d
+",j,i,j*i);
+        }
     }
-}
 }
