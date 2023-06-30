b73009ad88e5921b6a11d323fd83812da62ba480
varshith varma datla <119720179+7337232446@users.noreply.github.com>
2023-06-30 17:22:08 +0530

    #include<stdio.h> int main(){     int i,j,k,n;     scanf("%d",&n);     for(i=1;i<=n;i++){         for(j=1;j<=(n-2);j++){             printf("%d",j);         }         for(k=n-3;k>=1;k--){             printf("%d",k);         }         printf(" ");     } }

---
+++ print_the_following_pattern_8.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+int main(){
+    int i,j,k,n;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        for(j=1;j<=(n-2);j++){
+            printf("%d",j);
+        }
+        for(k=n-3;k>=1;k--){
+            printf("%d",k);
+        }
+        printf("
+");
+    }
+}
