fca701df6a15ef6d0cbacf167c677b98a72690a0
varshith varma datla <119720179+7337232446@users.noreply.github.com>
2023-06-30 17:38:30 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2==0 && i%2!=0)         {             printf("False");             return 0;         }     }     printf("True"); }

--- Strictly_EVEN.c
+++ Strictly_EVEN.c
@@ -6,31 +6,15 @@ int main()
     int a[n];
     for(i=0;i<n;i++)
     {
-       scanf("%d",&a[i]); 
-    }
-    int c=0;
-    int e=0;
-    for(int k=0;k<n;k++)
-    {
-        if(a[k]%2==0)
-        {
-            e++;
-        }
+        scanf("%d",&a[i]);
     }
     for(i=0;i<n;i++)
     {
-        if( i%2!=0 && a[i]%2==0 )
+        if(a[i]%2==0 && i%2!=0)
         {
-            c++;
+            printf("False");
+            return 0;
         }
     }
-    if(c==e)
-    {
-        printf("True");
-    }
-    else
-    {
-        printf("False");
-    }
-    
+    printf("True");
 }
